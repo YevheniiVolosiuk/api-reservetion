3ffd3555caedeb449c9f48a74b07d26f3756aa55
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-14 23:32:01 +0200

    add seeder for new permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Enums\Role as EnumRole;
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $allRoles = Role::all()->keyBy('id');
+
+        $permissions = [
+            'properties-manager' => [EnumRole::ROLE_OWNER],
+            'booking-manage' => [EnumRole::ROLE_USER],
+        ];
+
+        foreach ($permissions as $key => $roles) {
+            $permission = Permission::create(['name' => $key]);
+            foreach ($roles as $role) {
+                $allRoles[$role]->permissions()->attach($permission->id);
+            }
+        }
+    }
+
+}
