e00bcb1484682801eea01aff76ab68799f861b22
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-15 00:19:27 +0200

    define new route

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\API\Auth\RegisterController;
+use App\Http\Controllers\API\V1\Owner\PropertyController;
+use App\Http\Controllers\API\V1\User\BookingController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('auth/register', RegisterController::class);
 
-Route::middleware('auth:sanctum')->group(function (){
-
+Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
+    Route::get('owner/properties', [PropertyController::class, 'index']);
+    Route::get('user/bookings', [BookingController::class, 'index']);
 });
 
