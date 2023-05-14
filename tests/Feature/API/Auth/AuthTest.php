43397fbda877a749d86f54ea71382ee3609799fd
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-14 23:18:49 +0200

    add register tests

---
+++ tests/Feature/API/Auth/AuthTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace API\Auth;
+
+use App\Enums\Role;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_registration_fails_with_admin_role(): void
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_ADMINISTRATOR,
+        ]);
+
+        $response->assertStatus(422);
+    }
+
+    public function test_registration_succeeds_with_owner_role(): void
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_OWNER,
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+
+    public function test_registration_succeeds_with_user_role()
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'Valid name',
+            'email' => 'valid@email.com',
+            'password' => 'ValidPassword',
+            'password_confirmation' => 'ValidPassword',
+            'role_id' => Role::ROLE_USER,
+        ]);
+
+        $response->assertStatus(200)->assertJsonStructure([
+            'access_token',
+        ]);
+    }
+}
