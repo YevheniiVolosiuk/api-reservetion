23d98c9ef7247841edd366941af93fa75c4fceed
Yevhenii Volosiuk <volosiukweb@gmail.com>
2023-05-15 13:33:38 +0200

    with laravel-permissions package new changes

---
+++ tests/Feature/API/V1/PropertiesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\API\V1;
+
+use App\Enums\Role;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class PropertiesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_property_owner_has_access_to_properties_feature()
+    {
+        $owner = User::factory()->create()->assignRole(Role::ROLE_OWNER);
+        $response = $this->actingAs($owner)->getJson('/api/v1/owner/properties');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_does_not_have_access_to_properties_feature()
+    {
+        $user = User::factory()->create()->assignRole(Role::ROLE_USER);
+        $response = $this->actingAs($user)->getJson('/api/v1/owner/properties');
+
+        $response->assertStatus(403);
+    }
+}
